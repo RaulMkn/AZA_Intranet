38f1d9d01f632e157e721babb971b35ed09ed99f
RaulMkn <rmakendenguerodriguez@gmail.com>
2024-04-11 17:10:17 +0200

    Modificacion para ajustar los precios de las citas

--- DentalAesthetics_Backend/src/main/java/com/example/service/AppointmentService.java
+++ DentalAesthetics_Backend/src/main/java/com/example/service/AppointmentService.java
@@ -5,11 +5,13 @@ import com.example.dao.AppointmentDAO;
 import com.example.dao.impl.AppointmentDAOImpl;
 import com.example.entity.AppointmentEntity;
 import com.example.entity.DentistEntity;
+import com.example.entity.InterventionEntity;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -24,14 +26,28 @@ public class AppointmentService {
     public List<AppointmentEntity> getAllAppointments() {
         try (Session session = HibernateConfiguration.getSessionFactory().openSession()) {
             session.beginTransaction();
-            return appointmentDAO.getAllAppointmentsFromDatabase(session);
+            List<AppointmentEntity> appointments = appointmentDAO.getAllAppointmentsFromDatabase(session);
+            for (AppointmentEntity appointment : appointments) {
+                BigDecimal totalPrice = BigDecimal.ZERO;
+                for (InterventionEntity intervention : appointment.getInterventions()) {
+                    totalPrice = totalPrice.add(intervention.getPrice());
+                }
+                appointment.setTotal_price(totalPrice);
+            }
+            return appointments;
         }
     }
 
     public AppointmentEntity getAppointmentById(int id) {
         try (Session session = HibernateConfiguration.getSessionFactory().openSession()) {
             session.beginTransaction();
-            return appointmentDAO.getAppointmentFromDatabaseById(session, id);
+            AppointmentEntity appointment = appointmentDAO.getAppointmentFromDatabaseById(session, id);
+            BigDecimal totalPrice = BigDecimal.ZERO;
+            for (InterventionEntity intervention : appointment.getInterventions()) {
+                totalPrice = totalPrice.add(intervention.getPrice());
+            }
+            appointment.setTotal_price(totalPrice);
+            return appointment;
         }
     }
 
