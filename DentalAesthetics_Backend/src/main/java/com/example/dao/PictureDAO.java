294db38036048d8d9f6ada0755210ff7ef6f13db
RaulMkn <rmakendenguerodriguez@gmail.com>
2024-05-06 14:56:16 +0200

    Cambiado nombre del proyecto

--- DentalAesthetics_Backend/src/main/java/com/example/dao/PictureDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.dao;
-
-import com.example.entity.PictureEntity;
-import org.hibernate.Session;
-
-public interface PictureDAO {
-    PictureEntity persistPictureToDatabase(PictureEntity pictureAttached, Session session);
-}
