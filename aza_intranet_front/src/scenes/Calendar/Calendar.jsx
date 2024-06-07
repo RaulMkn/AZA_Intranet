264c99bef87441bb777f99195b16061ae4bd427e
RaulMkn <rmakendenguerodriguez@gmail.com>
2024-06-07 04:20:20 +0200

    Modificada la forma de mostrar info en el calendario

--- aza_intranet_front/src/scenes/Calendar/Calendar.jsx
+++ aza_intranet_front/src/scenes/Calendar/Calendar.jsx
@@ -1,89 +1,83 @@
 import { useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 import axios from "axios";
 import esLocale from "@fullcalendar/core/locales/es";
 import { checkPermissionsAndRedirect } from "../../utils/CheckPermissions";
+import Swal from "sweetalert2";
 
 function Calendar() {
-  var dentistJson = localStorage.getItem("Dentist");
-  console.log(dentistJson);
-  var dentistDto = JSON.parse(dentistJson);
+  const dentistJson = localStorage.getItem("Dentist");
+  const dentistDto = dentistJson ? JSON.parse(dentistJson) : null;
   const [eventos, setEventos] = useState([]);
 
   useEffect(() => {
-    checkPermissionsAndRedirect(dentistDto);
-  });
+    if (dentistDto) {
+      checkPermissionsAndRedirect(dentistDto);
+    } 
+  }, [dentistDto]);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:8080/intranet/DentalAesthetics/appointment/dentistId/${dentistDto.id}`)
-      .then((response) => {
-        const formattedEvents = response.data.map((evento) => ({
-          title: evento.title,
-          start: evento.date_time_beginning,
-          end: evento.date_time_ending,
-          description: evento.description,
-          patient: evento.patient,
-        }));
-        setEventos(formattedEvents);
-      })
-      .catch((error) => {
-        console.error("Error al obtener eventos:", error);
-      });
-  }, []); // Se ejecuta solo al montar el componente
+    if (dentistDto) {
+      axios
+        .get(`http://localhost:8080/intranet/DentalAesthetics/appointment/dentistId/${dentistDto.id}`)
+        .then((response) => {
+          const formattedEvents = response.data.map((evento) => ({
+            title: evento.title,
+            start: evento.date_time_beginning,
+            end: evento.date_time_ending,
+            description: evento.description,
+            patient: evento.patient,
+          }));
+          setEventos(formattedEvents);
+        })
+        .catch((error) => {
+          console.error("Error al obtener eventos:", error);
+        });
+    }
+  }, []);
 
-  const eventMouseEnter = ({ el, event }) => {
-    el.classList.add("relative");
-
-    const newEl = document.createElement("div");
-    newEl.innerHTML = `
-      <div class="fc-hoverable-event" style="position: absolute; bottom: 100%; width: 300px; height: auto; background-color: white; z-index: 50; border: 1px solid #e2e8f0; border-radius: 0.375rem; padding: 0.75rem; font-size: 14px; font-family: 'Inter', sans-serif; cursor: pointer;">
-        <strong>${event.title}</strong>
-        <div><strong>Descripcion:</strong> ${event.extendedProps.description}</div>
-        <div><strong>Paciente:</strong> ${event.extendedProps.patient.full_name}</div>
-        <div><strong>Inicio:</strong> ${new Date(event.start).toLocaleTimeString()}</div>
-        <div><strong>Final:</strong> ${new Date(event.end).toLocaleTimeString()}</div>
-      </div>
-    `;
-    el.after(newEl);
-  };
-
-  const eventMouseLeave = () => {
-    document.querySelector(".fc-hoverable-event").remove();
+  const eventClick = (info) => {
+    Swal.fire({
+      icon: "info",
+      title: `${info.event.title}`,
+      html: `
+        <div><strong>Descripción:</strong> ${info.event.extendedProps.description}</div>
+        <div><strong>Paciente:</strong> ${info.event.extendedProps.patient.full_name}</div>
+        <div><strong>Inicio:</strong> ${new Date(info.event.start).toLocaleTimeString()}</div>
+        <div><strong>Final:</strong> ${new Date(info.event.end).toLocaleTimeString()}</div>
+      `,
+    });
   };
 
   return (
-    <>
-      <div
-        style={{
-          width: "1050px",
-          height: "90vh",
-          color: "black",
-          marginLeft: "9%",
-          marginRight: "0",
+    <div
+      style={{
+        width: "1050px",
+        height: "90vh",
+        color: "black",
+        marginLeft: "9%",
+        marginRight: "0",
+      }}
+    >
+      <FullCalendar
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
         }}
-      >
-        <FullCalendar
-          headerToolbar={{
-            left: "prev,next today",
-            center: "title",
-            right: 'dayGridMonth,timeGridWeek,timeGridDay'
-          }}
-          initialView='timeGridWeek'
-          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-          events={eventos}
-          eventMouseEnter={eventMouseEnter}
-          eventMouseLeave={eventMouseLeave}
-          height={"83vh"}
-          weekends={false}
-          locale={esLocale}
-          firstDay={1}
-        />
-      </div>
-    </>
+        initialView='timeGridWeek'
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        events={eventos}
+        eventClick={eventClick}
+        height={"83vh"}
+        weekends={false}
+        locale={esLocale}
+        firstDay={1}
+      />
+    </div>
   );
 }
 
--- aza_intranet_front/src/scenes/Home/HomeProfile.jsx
+++ aza_intranet_front/src/scenes/Home/HomeProfile.jsx
@@ -14,24 +14,25 @@ const Home = () => {
   }
 
   return (
-    <Card title="Información del Dentista" style={{ width: 600, margin: 'auto', marginTop: '50px' }}>
+    <Card title="Tus datos personales" style={{ width: 600, margin: 'auto', marginTop: '50px' }}>
+            <Avatar
+        src={`data:${dentist.picture.img_type};base64,${dentist.picture.img}`}
+        size={128}
+        style={{ marginTop: '20px' }}
+      />
       <Descriptions bordered column={1}>
         <Descriptions.Item label="Nombre Completo">{dentist.full_name}</Descriptions.Item>
         <Descriptions.Item label="Correo Electrónico">{dentist.email}</Descriptions.Item>
         <Descriptions.Item label="Fecha de Nacimiento">{dentist.date_of_birth}</Descriptions.Item>
         <Descriptions.Item label="Género">{dentist.gender}</Descriptions.Item>
+        <Descriptions.Item label="Departamento">{dentist.department.department_name}</Descriptions.Item>
         <Descriptions.Item label="Trabajo">{dentist.job}</Descriptions.Item>
         <Descriptions.Item label="NIF">{dentist.nif}</Descriptions.Item>
-        <Descriptions.Item label="Departamento">{dentist.department.department_name}</Descriptions.Item>
         <Descriptions.Item label="Permisos">{dentist.permits}</Descriptions.Item>
         <Descriptions.Item label="Dirección">{dentist.address}</Descriptions.Item>
         <Descriptions.Item label="ID">{dentist.id}</Descriptions.Item>
       </Descriptions>
-      <Avatar
-        src={`data:${dentist.picture.img_type};base64,${dentist.picture.img}`}
-        size={128}
-        style={{ marginTop: '20px' }}
-      />
+
     </Card>
   );
 };
